1cf3afc37ad30c6a899ecbd05d24199d335c7e3c
Cupmaster <gosha.tswetkov@gmail.com>
2026-03-14 22:29:09 +0300

    Removed the window's ability to be resized, which would break the UI. Also hopefully fixed the commit mess that I had before.

--- src/main/java/org/example/txtfilemaker/HelloApplication.java
+++ src/main/java/org/example/txtfilemaker/HelloApplication.java
@@ -14,6 +14,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 800, 800);
         stage.setTitle("File Generator");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
